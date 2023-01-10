b58555265a7473dc8a464a89c9b9b26cf7e3176d
Evgeniy <volhin.z@mail.ru>
2023-01-10 15:02:45 +0300

    All work

--- src/main/java/com/example/shop/controller/CategoController.java
+++ src/main/java/com/example/shop/controller/CategoController.java
@@ -45,36 +45,4 @@ public class CategoController {
 
 
     }
-    /*
-
-
-    @PostMapping
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
-        Message message = new Message(text, tag);
-
-        messageRepo.save(message);
-
-        Iterable<Message> messages = messageRepo.findAll();
-
-        model.put("messages", messages);
-
-        return "main";
-    }
-
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter, Map<String, Object> model) {
-        Iterable<Message> messages;
-
-        if (filter != null && !filter.isEmpty()) {
-            messages = messageRepo.findByTag(filter);
-        } else {
-            messages = messageRepo.findAll();
-        }
-
-        model.put("messages", messages);
-
-        return "main";
-    }
-
-    */
 }
